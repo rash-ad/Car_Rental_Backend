7d478b8804a034792f93ef0d38c9a7d425daf9f1
rash-ad <Rashad.mohd2004@gmail.com>
2026-05-14 18:03:06 +0530

    feat: add Car model and CarController and repository interface

--- src/main/java/edu/icet/ecom/controller/CarController.java
+++ src/main/java/edu/icet/ecom/controller/CarController.java
@@ -1,4 +1,26 @@
 package edu.icet.ecom.controller;
 
+import edu.icet.ecom.dto.CarResponse;
+import edu.icet.ecom.service.CarService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/cars")
 public class CarController {
-}
+        @Autowired
+        private CarService carService;
+
+        @GetMapping
+        public List<CarResponse> getAllCars() {
+            return carService.getAllCars();
+        }
+
+    }
+
